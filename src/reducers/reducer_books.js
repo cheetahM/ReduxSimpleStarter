c32363e3c967584772b78c625b4d390b02fd7e4e
Chetan Meshram <meshram.chetan@gmail.com>
2016-05-27 16:23:30 -0700

    creating a container class Booklist

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title: 'Javascript: The good parts'},
+    {title: "The abc"},
+    {title: "Real madrid"},
+    {title:" Machester United"},
+    {title:" Let the game begin"}
+  ]
+}
