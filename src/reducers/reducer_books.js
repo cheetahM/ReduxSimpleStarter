4fc844fdde9a3f373e9269ad64582fb2a77fe2ab
cmeshram <meshram.chetan@gmail.com>
2016-05-30 23:19:49 -0700

    adding a redux container and using connect to pass app  state to react component

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    {title: 'Javascript: The good parts'},
-    {title: "The abc"},
-    {title: "Real madrid"},
-    {title:" Machester United"},
-    {title:" Let the game begin"}
+    {title: 'Javascript: The good parts', pages: 300},
+    {title: "The abc", pages:30},
+    {title: "Real madrid", pages:100},
+    {title:" Machester United", pages:400},
+    {title:" Let the game begin", pages:50}
   ]
 }
